5c155db22e7353a62493f4e5b1620943c9994fd2
Fatih Akca <fatihakca5@gmail.com>
2018-11-08 22:02:00 +0300

    Implement custom exceptions for controllers: ForbiddenException, NotFoundException

---
+++ server/src/main/java/com/akson/invaders/server/util/ForbiddenException.java
@@ -0,0 +1,12 @@
+package com.akson.invaders.server.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException {
+
+    public ForbiddenException(String msg) {
+        super(msg);
+    }
+}
