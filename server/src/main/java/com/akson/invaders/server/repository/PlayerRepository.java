f0771586facbc4893f817d3ed039c6ccfcbb4cf3
Fatih Akca <fatihakca5@gmail.com>
2018-11-10 23:06:00 +0300

    Add PlayerRepository

---
+++ server/src/main/java/com/akson/invaders/server/repository/PlayerRepository.java
@@ -0,0 +1,9 @@
+package com.akson.invaders.server.repository;
+
+import com.akson.invaders.server.entity.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+}
