615f267a56ba895b370c653f58b91211107978c3
Fatih Akca <fatihakca5@gmail.com>
2018-12-03 20:10:19 +0300

    Refactor things a little bit

--- client/src/main/java/com/akson/invaders/client/SpaceInvadersApp.java
+++ client/src/main/java/com/akson/invaders/client/SpaceInvadersApp.java
@@ -5,11 +5,20 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,9 +31,11 @@ public class SpaceInvadersApp extends Application {
     public static final int WINDOW_HEIGHT = 800;
     public static final int PLAYER_SIZE = 40;
     public static final int ENEMY_SIZE = 30;
-    private static Logger logger = LoggerFactory.getLogger(SpaceInvadersApp.class);
+
+    private static final Logger logger = LoggerFactory.getLogger(SpaceInvadersApp.class);
+
+    private Stage stage;
     private Pane root = new Pane();
-    private double t = 0;
 
     private GameObject playerObject;
     private StateManager stateManager;
@@ -39,117 +50,17 @@ public class SpaceInvadersApp extends Application {
         launch(args);
     }
 
-    private Parent createContent() {
-
-        stateManager = new StateManagerSP(this);
-
-        playerObject = new GameObject(GameObjectType.LOCAL_PLAYER, 300, 700);
-
-        Sprite playerSprite = new Sprite(playerObject,
-                playerObject.getX(),
-                playerObject.getY(),
-                PLAYER_SIZE,
-                PLAYER_SIZE,
-                Color.BLUE);
-
-        stateManager.addObject(playerObject);
-
-        AnimationTimer timer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                if (leftPressed.get() && playerObject.getX() > 0) {
-                    stateManager.moveHorizontal(playerObject.getId(), -4);
-                }
-                if (rightPressed.get() && playerObject.getX() + PLAYER_SIZE < root.getWidth()) {
-                    stateManager.moveHorizontal(playerObject.getId(), 4);
-                }
-                if (upPressed.get() && playerObject.getY() > 0) {
-                    stateManager.moveVertical(playerObject.getId(), -4);
-                }
-                if (downPressed.get() && playerObject.getY() + PLAYER_SIZE < root.getHeight()) {
-                    stateManager.moveVertical(playerObject.getId(), 4);
-                }
-                if (spacePressed.get()) {
-                    stateManager.shoot(playerObject);
-                }
-            }
-        };
-
-        timer.start();
-        createAliens();
-
-        return root;
-    }
-//
-//    private List<Sprite> sprites() {
-//        return root.getChildren().stream().map(n -> (Sprite) n).collect(Collectors.toList());
-//    }
-
-    private void createAliens() {
-        for (int i = 0; i < 5; i++) {
-
-            GameObject alienObject = new GameObject(GameObjectType.ENEMY, 90 + i * 100, 100);
-
-            Sprite alienSprite = new Sprite(alienObject,
-                    alienObject.getX(),
-                    alienObject.getY(),
-                    ENEMY_SIZE,
-                    ENEMY_SIZE,
-                    Color.RED);
-
-            stateManager.addObject(alienObject);
-
-        }
-    }
-
     @Override
     public void start(Stage stage) throws Exception {
+
+        this.stage = stage;
 
         stage.setWidth(WINDOW_WIDTH);
         stage.setHeight(WINDOW_HEIGHT);
         stage.setResizable(false);
 
-        Scene scene = new Scene(createContent());
+        switchToMainMenuScene();
 
-        scene.setOnKeyPressed(e -> {
-            if (e.getCode() == UP) {
-                upPressed.set(true);
-            }
-            if (e.getCode() == DOWN) {
-                downPressed.set(true);
-            }
-            if (e.getCode() == LEFT) {
-                leftPressed.set(true);
-            }
-            if (e.getCode() == RIGHT) {
-                rightPressed.set(true);
-            }
-            if (e.getCode() == SPACE) {
-                spacePressed.set(true);
-            }
-        });
-
-        scene.setOnKeyReleased(e -> {
-            if (e.getCode() == UP) {
-                upPressed.set(false);
-            }
-            if (e.getCode() == DOWN) {
-                downPressed.set(false);
-            }
-            if (e.getCode() == LEFT) {
-                leftPressed.set(false);
-            }
-            if (e.getCode() == RIGHT) {
-                rightPressed.set(false);
-            }
-            if (e.getCode() == SPACE) {
-                spacePressed.set(false);
-            }
-        });
-
-        stage.setScene(scene);
-        stage.show();
-        stateManager.start();
     }
 
     private Sprite findSpriteByGameObject(GameObject object) {
@@ -194,5 +105,168 @@ public class SpaceInvadersApp extends Application {
 
     public GameObject getPlayerObject() {
         return playerObject;
+    }
+
+
+    private Parent createMainMenuUI() {
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(0, 10, 0, 10));
+        grid.setAlignment(Pos.CENTER);
+
+        DropShadow ds = new DropShadow();
+        ds.setRadius(15);
+        ds.setSpread(0.99);
+        ds.setColor(Color.color(0.1, 0.1, 0.1));
+
+        Text header = new Text("INVADERS!");
+        header.setFont(Font.font("Verdana", FontWeight.BOLD, 70));
+        header.setFill(Color.RED);
+        header.setEffect(ds);
+
+        Button singlePlayerButton = new Button("Singleplayer");
+        Button multiPlayerButton = new Button("Multiplayer");
+        Button highScoresButton = new Button("Highscores");
+        Button exitButton = new Button("Exit");
+
+        singlePlayerButton.setMinWidth(300);
+        multiPlayerButton.setMinWidth(300);
+        highScoresButton.setMinWidth(300);
+        exitButton.setMinWidth(300);
+
+        GridPane.setHalignment(singlePlayerButton, HPos.CENTER);
+        GridPane.setHalignment(multiPlayerButton, HPos.CENTER);
+        GridPane.setHalignment(highScoresButton, HPos.CENTER);
+        GridPane.setHalignment(exitButton, HPos.CENTER);
+
+        singlePlayerButton.setEffect(ds);
+        multiPlayerButton.setEffect(ds);
+        highScoresButton.setEffect(ds);
+        exitButton.setEffect(ds);
+
+        singlePlayerButton.setOnAction(e -> switchToGameScene());
+
+        exitButton.setOnAction(e -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
+        grid.add(header, 0, 0);
+        grid.add(singlePlayerButton, 0, 6);
+        grid.add(multiPlayerButton, 0, 7);
+        grid.add(highScoresButton, 0, 8);
+        grid.add(exitButton, 0, 9);
+
+        return grid;
+    }
+
+    private void switchToMainMenuScene() {
+
+        Scene mainScene = new Scene(createMainMenuUI());
+
+        stage.setScene(mainScene);
+        stage.show();
+    }
+
+    private void switchToGameScene() {
+
+        Scene gameScene = new Scene(root);
+        stateManager = new StateManagerSP(this);
+        playerObject = new GameObject(GameObjectType.LOCAL_PLAYER, 300, 700);
+
+        Sprite playerSprite = new Sprite(playerObject,
+                playerObject.getX(),
+                playerObject.getY(),
+                PLAYER_SIZE,
+                PLAYER_SIZE,
+                Color.BLUE);
+
+        stateManager.addObject(playerObject);
+
+
+        gameScene.setOnKeyPressed(e -> {
+            if (e.getCode() == UP) {
+                upPressed.set(true);
+            }
+            if (e.getCode() == DOWN) {
+                downPressed.set(true);
+            }
+            if (e.getCode() == LEFT) {
+                leftPressed.set(true);
+            }
+            if (e.getCode() == RIGHT) {
+                rightPressed.set(true);
+            }
+            if (e.getCode() == SPACE) {
+                spacePressed.set(true);
+            }
+        });
+
+        gameScene.setOnKeyReleased(e -> {
+            if (e.getCode() == UP) {
+                upPressed.set(false);
+            }
+            if (e.getCode() == DOWN) {
+                downPressed.set(false);
+            }
+            if (e.getCode() == LEFT) {
+                leftPressed.set(false);
+            }
+            if (e.getCode() == RIGHT) {
+                rightPressed.set(false);
+            }
+            if (e.getCode() == SPACE) {
+                spacePressed.set(false);
+            }
+        });
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (leftPressed.get() && playerObject.getX() > 0) {
+                    stateManager.moveHorizontal(playerObject.getId(), -4);
+                }
+                if (rightPressed.get() && playerObject.getX() + PLAYER_SIZE < root.getWidth()) {
+                    stateManager.moveHorizontal(playerObject.getId(), 4);
+                }
+                if (upPressed.get() && playerObject.getY() > 0) {
+                    stateManager.moveVertical(playerObject.getId(), -4);
+                }
+                if (downPressed.get() && playerObject.getY() + PLAYER_SIZE < root.getHeight()) {
+                    stateManager.moveVertical(playerObject.getId(), 4);
+                }
+                if (spacePressed.get()) {
+                    stateManager.shoot(playerObject);
+                }
+            }
+        };
+
+        createAliens();
+        timer.start();
+        stateManager.start();
+
+        stage.setScene(gameScene);
+        stage.show();
+
+    }
+
+    private void createAliens() {
+
+        for (int i = 0; i < 5; i++) {
+
+            GameObject alienObject = new GameObject(GameObjectType.ENEMY, 90 + i * 100, 100);
+
+            Sprite alienSprite = new Sprite(alienObject,
+                    alienObject.getX(),
+                    alienObject.getY(),
+                    ENEMY_SIZE,
+                    ENEMY_SIZE,
+                    Color.RED);
+
+            stateManager.addObject(alienObject);
+
+        }
     }
 }
