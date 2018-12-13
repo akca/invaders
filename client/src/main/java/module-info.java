0b74027fe423d91958bf31ef23cb339196351d51
Fatih Akca <fatihakca5@gmail.com>
2018-12-13 21:40:16 +0300

    Declare required modules in module-info.java

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,9 +1,16 @@
 open module invaders.client {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
     requires spring.boot.autoconfigure;
     requires spring.boot;
     requires spring.context;
     requires spring.beans;
+    requires spring.hateoas;
+    requires spring.core;
     requires slf4j.api;
+    requires spring.web;
+    requires org.apache.httpcomponents.httpclient;
+    requires org.apache.httpcomponents.httpcore;
+    requires invaders.common;
 }
